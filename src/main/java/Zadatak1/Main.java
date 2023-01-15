1a394f7d105bfa8eb3e0991fd4287c7e7ca24f6e
bojanradivojevic <bojanradivojevic1503@gmail.com>
2023-01-15 18:04:43 +0100

    Ucitavanje fajla

--- src/main/java/Zadatak1/Main.java
+++ src/main/java/Zadatak1/Main.java
@@ -1,8 +1,23 @@
 package Zadatak1;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Imena i prezimena polaznika");
-        
+
+        try {
+            FileInputStream inputStream = new FileInputStream("ImenaIPrezimena");
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = workbook.getSheet("Sheet1");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
